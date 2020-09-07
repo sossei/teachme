e17e568ff860841f4f2a76f27a3ab4fbff00cf6c
Vinicius Sossei Sakugawa <viny.sossei@icloud.com>
2020-09-07 12:58:56 -0300

    Initial Commit

---
+++ TeachMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeachMe
+//
+//  Created by Vinicius Sossei on 07/09/20.
+//  Copyright © 2020 Sossei Tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
